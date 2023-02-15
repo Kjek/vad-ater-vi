60b4a6243982f6101d84542a3f60d96f6c85f032
Kjek <kjek@me.com>
2023-02-15 11:03:46 +0100

    Added bryners web scraper

---
+++ src/server/scrapers/bryners.ts
@@ -0,0 +1,36 @@
+import puppeteer from "puppeteer";
+
+const bryners = async () => {
+  console.log("Hämtar bryners meny!");
+
+  const browser = await puppeteer.launch({ headless: true });
+  const page = await browser.newPage();
+
+  await page.goto("https://bryners.se/veckans-lunch-v-j/bryners-bistro.html", {
+    waitUntil: "networkidle0",
+  });
+
+  const lunchMenu = await page.evaluate(() => {
+    const days = ["Måndag", "Tisdag", "Onsdag", "Torsdag", "Fredag"];
+    const menuItems = Array.from(document.querySelectorAll("ul"))
+      .filter(
+        (ul) =>
+          ul.previousElementSibling?.textContent !== undefined &&
+          ul.previousElementSibling?.textContent !== null &&
+          days.includes(ul.previousElementSibling?.textContent.split(" ")[0])
+      )
+      .map((item) => {
+        const title = item.previousElementSibling?.textContent;
+        const menu = item.textContent;
+        return { title, menu };
+      });
+
+    return menuItems;
+  });
+
+  console.debug(lunchMenu);
+
+  await browser.close();
+};
+
+export default bryners;
