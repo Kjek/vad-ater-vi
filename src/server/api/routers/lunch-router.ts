46370dd108be63ec59fb6d3c72c4e02946348067
Kjek <kjek@me.com>
2023-03-19 13:30:05 +0100

    Removed dumb code

--- src/server/api/routers/lunch-router.ts
+++ src/server/api/routers/lunch-router.ts
@@ -1,6 +1,5 @@
 import { z } from 'zod';
 import { createTRPCRouter, publicProcedure } from '~/server/api/trpc';
-import augustasWebScraper from '~/server/scrapers/augustas';
 import {
   handleLunchScrapers,
   handleLunchSearch,
@@ -8,7 +7,6 @@ import {
 
 export const lunchRouter = createTRPCRouter({
   menu: publicProcedure.query(async ({ ctx }) => {
-    await augustasWebScraper();
     return await handleLunchScrapers(ctx.prisma);
   }),
   menuSearch: publicProcedure
