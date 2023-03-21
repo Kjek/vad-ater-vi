df3cf09174d702dd0032809cc1eb22c3b02a3df0
Kjek <Kjek@users.noreply.github.com>
2023-03-21 18:59:45 +0100

    Added image & favicon; Added some error management (#20)

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,3 +1,4 @@
+import Image from 'next/image';
 import GitHubIcon from '~/assets/icons/GitHubIcon';
 import Switcher from './Switcher';
 
@@ -5,8 +6,14 @@ const Header = () => {
   return (
     <header>
       <nav className='flex justify-between bg-neutral-100 py-2 px-4 shadow-md transition duration-500 dark:border-spacing-1 dark:border-b dark:border-gray-700 dark:bg-gray-900 dark:shadow-none'>
-        <div className='m-auto flex w-full grow justify-center'>
-          <h1 className='text-3xl font-bold text-gray-800 dark:text-gray-300 sm:ml-20'>
+        <div className='m-auto flex w-full grow justify-center gap-4 sm:ml-20 '>
+          <Image
+            src='/site-image.png'
+            alt='Lunch Image'
+            width={40}
+            height={40}
+          />
+          <h1 className='text-3xl font-bold text-gray-800 dark:text-gray-300'>
             Lunch i Sundsvall
           </h1>
         </div>
