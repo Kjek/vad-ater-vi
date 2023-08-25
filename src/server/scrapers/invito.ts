e3da94c61e9fd90b669d4ace0bf764518fabbed1
Kjek <kjek@me.com>
2023-08-25 08:14:59 +0200

    Fixed invito lunch menu

--- src/server/scrapers/invito.ts
+++ src/server/scrapers/invito.ts
@@ -8,21 +8,16 @@ const invitoWebScraper: Scraper = async (lunchUrl, regex) => {
   const html = await (await fetch(lunchUrl)).text();
   const { document } = parseHTML(html);
 
-  const lunchMenu = Array.from(
-    document.querySelectorAll('#meny-objekt-veckans-lunch-ul')
-  )
-    .flatMap((ul) => Array.from(ul.querySelectorAll('li')))
-    .flatMap(
-      (li) =>
-        ({
-          day: li.querySelector('p')?.textContent?.trim(),
-          food: li
-            .querySelector('.beskrivning')
-            ?.textContent?.replaceAll('\t', '')
-            .replace('\n', '')
-            .trim(),
-        } as LunchMenu)
-    );
+  const matched = document
+    .querySelector('#lunchmeny')
+    ?.nextElementSibling?.querySelector('div')
+    ?.textContent?.matchAll(regex ?? /^([a-öA-Ö]+) \n+(.*)/gm);
+
+  const lunchMenu: LunchMenu[] = [];
+
+  for (const text of matched ?? []) {
+    lunchMenu.push({ day: text[1], food: text[2] } as LunchMenu);
+  }
 
   console.timeEnd('Fetching Invito menu');
   return lunchMenu;
